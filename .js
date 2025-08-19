26c1287d4e624c6985ba098af1d3b80e828850cd
Rishi Ganeshan <rishi9aneshan@gmail.com>
2025-08-19 14:54:33 +0100

    Stop the game after one player reaches 5 points

--- .js
+++ .js
@@ -36,42 +36,52 @@ function getHumanChoice(choice) {
 
 let humanScore = 0;
 let computerScore = 0;
+let div = document.querySelector("div")
+
 
 function playRound(humanChoice) {
     humanChoice = getHumanChoice(humanChoice);
     let computerChoice = getComputerChoice();
-    console.log(humanChoice)
+    // console.log(humanChoice)
+    
+    let roundSummary = document.createElement("div")
+    
 
     if (humanChoice === "Rock") {
         if (computerChoice === "Rock") {
-            console.log("It's a tie!");
+            roundSummary.textContent = "It's a tie!"
 
 
         } else if (computerChoice === "Paper") {
-            console.log("You lose! Paper beats Rock");
+            roundSummary.textContent = "You lose! Paper beats Rock"
+
             computerScore++;
 
 
         } else if (computerChoice === "Scissors") {
-            console.log("You win! Rock beats Scissors");
+            roundSummary.textContent = "You win! Rock beats Scissors"
+
             humanScore++;
 
 
         }
     } else if (humanChoice === "Paper") {
         if (computerChoice === "Rock") {
-            console.log("You win! Paper beats Rock");
+            roundSummary.textContent = "You win! Paper beats Rock"
+
             humanScore++;
 
 
 
         } else if (computerChoice === "Paper") {
-            console.log("It's a tie!");
+            roundSummary.textContent = "It's a tie!"
+
 
 
 
         } else if (computerChoice === "Scissors") {
-            console.log("You lose! Scissors beats Paper");
+            roundSummary.textContent = "You lose! Scissors beats Paper"
+
             computerScore++;
 
 
@@ -79,27 +89,70 @@ function playRound(humanChoice) {
 
     } else if (humanChoice === "Scissors") {
         if (computerChoice === "Rock") {
-            console.log("You lose! Rock beats Scissors");
+            roundSummary.textContent = "You lose! Rock beats Scissors"
+
             computerScore++;
 
 
         } else if (computerChoice === "Paper") {
-            console.log("You win! Scissors beats Paper");
+            roundSummary.textContent = "You win! Scissors beats Paper"
+
             humanScore++;
 
 
 
         } else if (computerChoice === "Scissors") {
-            console.log("It's a tie!");
+            roundSummary.textContent = "It's a tie!"
+
 
 
         }
     }
-    console.log("Computer Score: " + computerScore);
-    console.log("Your Score: " + humanScore);
-    console.log()
+
+    if (humanScore === 5 || computerScore === 5) {
+        finishGame()
 
 
+    } else {
+        // roundSummary.textContent = "It's a tie!"
+        let computerScoreText = document.createElement("div")
+        let humanScoreText = document.createElement("div")
+        computerScoreText.textContent = "Computer Score: " + computerScore
+        humanScoreText.textContent = "Your Score: " + humanScore
+
+
+
+
+        div.appendChild(roundSummary)
+        div.appendChild(computerScoreText)
+        div.appendChild(humanScoreText)
+        let newline = document.createElement("div")
+        newline.textContent = "------------------------------------"
+        div.appendChild(newline)
+
+    }
+
+
+    
+    
+
+
+
+}
+
+function finishGame() {
+    let text = document.createElement("div")
+    if (humanScore === 5) {
+        text.textContent = "Congratulations. You reached 5 points first!"
+    } else {
+        text.textContent = "The computer reached 5 points first! Better luck next time.s"
+    }
+    div.appendChild(text)
+
+    buttons.forEach((button) => {
+        button.remove()
+        
+    })
 }
 
 
@@ -111,4 +164,6 @@ buttons.forEach((button) => {
         playRound(button.currentTarget.textContent);
     })
 });
+
+
 
